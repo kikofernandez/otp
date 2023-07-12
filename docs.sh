96710fc76d6980b7b6aeec3c07d85678df277c29
Lukas Larsson <lukas@erlang.org>
2023-07-12 14:41:13 +0200

    Fix things

--- docs.sh
+++ docs.sh
@@ -11,13 +11,19 @@ function is_bin_in_path {
 }
 
 if ! is_bin_in_path iex; then
-    git clone -b main https://github.com/elixir-lang/elixir
-    (cd elixir && make)
+    if [ ! -d elixir ]; then
+        git clone -b main https://github.com/elixir-lang/elixir
+        (cd elixir && make)
+    fi
     export PATH=`pwd`/elixir/bin:$PATH
 fi
 
 if ! is_bin_in_path ex_doc; then
-    git clone -b lukas/fix-erlang-doc-support https://github.com/garazdawi/ex_doc
+    if [ ! -d ex_doc ]; then
+        git clone -b lukas/fix-erlang-doc-support https://github.com/garazdawi/ex_doc
+    else
+        (cd ex_doc && git pull origin lukas/fix-erlang-doc-support)
+    fi
     (cd ex_doc && mix deps.get && mix escript.build)
     export PATH=`pwd`/ex_doc/:$PATH
 fi
@@ -27,7 +33,7 @@ for app in $(ls -d lib/${APPS}/ebin | awk -F "/" '{print $2}'); do
     echo "Creating docs for $app"
     VSN=$(cat lib/$app/vsn.mk | grep -i "^${app}_vsn" | awk '{print $3}')
     erl -noinput -eval "docgen_xml_to_markdown:convert_application(${app}), halt()"
-    ./insert_chunks lib/$app/ebin/*.beam
+    ./insert_chunks.es lib/$app/ebin/*.beam
     APP="lib/$app" ex_doc $app "${VSN}" "lib/$app/ebin" -o "docs/$app" -c ex_doc.exs || exit
 done
 
@@ -35,6 +41,6 @@ if [ "$APPS" = '*' ] || [ $APPS = 'erts' ]; then
     app=erts
     VSN=$(cat $app/vsn.mk | grep -i "^VSN" | awk '{print $3}')
     erl -noinput -eval "docgen_xml_to_markdown:convert_application(${app}), halt()"
-    ./insert_chunks $app/preloaded/ebin/*.beam
+    ./insert_chunks.es $app/preloaded/ebin/*.beam
     APP=$app ex_doc $app "${VSN}" "$app/preloaded/ebin" -o "docs/$app" -c ex_doc.exs || exit
 fi
