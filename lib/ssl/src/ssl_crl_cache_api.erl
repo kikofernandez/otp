b18061568316855b1ec6567e9c30029ac034d9da
Lukas Larsson <lukas@erlang.org>
2023-07-28 15:03:44 +0200

    Add optional lookup/2 callback

--- lib/ssl/src/ssl_crl_cache_api.erl
+++ lib/ssl/src/ssl_crl_cache_api.erl
@@ -30,9 +30,13 @@
 -type dist_point()  :: #'DistributionPoint'{}.
 -type logger_info()     :: {logger:level(), Report::#{description => string(), reason => term()}, logger:metadata()}.
 
+-callback lookup(dist_point(), crl_cache_ref()) -> not_available | [public_key:der_encoded()] | 
+                                                   {{logger, logger_info()}, [public_key:der_encoded()]}.
 -callback lookup(dist_point(), issuer_name(), crl_cache_ref()) -> not_available | [public_key:der_encoded()] | 
                                                                   {{logger, logger_info()}, [public_key:der_encoded()]}.
 -callback select(issuer_name() | list(), crl_cache_ref()) ->  [public_key:der_encoded()] |  
                                                      {logger, logger_info(), [public_key:der_encoded()]}.
 -callback fresh_crl(dist_point(), public_key:der_encoded()) -> public_key:der_encoded() | 
                                                                {logger, logger_info(),  public_key:der_encoded()}.
+
+-optional_callbacks([lookup/2]).
